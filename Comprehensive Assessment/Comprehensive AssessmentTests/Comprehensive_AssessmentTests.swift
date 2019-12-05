f89771660b56c7ecf88d3192f8c3b89a9edd7824
Michelle <Michelle.cueva@gmail.com>
2019-12-05 15:22:49 -0500

    Tests models

--- Comprehensive Assessment/Comprehensive AssessmentTests/Comprehensive_AssessmentTests.swift
+++ Comprehensive Assessment/Comprehensive AssessmentTests/Comprehensive_AssessmentTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+//@testable import Comprehensive_Assessment
 
 class Comprehensive_AssessmentTests: XCTestCase {
 
@@ -24,10 +25,87 @@ class Comprehensive_AssessmentTests: XCTestCase {
     }
 
     func testPerformanceExample() {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+      
     }
+    
+    private func TicketModel() -> Data? {
+             let bundle = Bundle(for: type(of: self))
+             guard let pathToData = bundle.path(forResource: "TicketmasterSample", ofType: ".json")  else {
+                 XCTFail("couldn't find Json")
+                 return nil
+             }
+             let url = URL(fileURLWithPath: pathToData)
+             do {
+                 let data = try Data(contentsOf: url)
+                 return data
+             } catch let error {
+                 fatalError("couldn't find data \(error)")
+             }
+         }
+    
+    func testTicketModel () {
+            let data = TicketModel() ?? Data()
+            
+            do {
+                let ticketData = try TicketWrapper.decodeEventFromData(from: data)
+                
+                 XCTAssertTrue(ticketData.count > 0, "events not loaded")
+            } catch let error {
+                XCTFail(error.localizedDescription)
+            }
+    }
+    
+    private func MuseumModel() -> Data? {
+               let bundle = Bundle(for: type(of: self))
+               guard let pathToData = bundle.path(forResource: "MuseumSample", ofType: ".json")  else {
+                   XCTFail("couldn't find Json")
+                   return nil
+               }
+               let url = URL(fileURLWithPath: pathToData)
+               do {
+                   let data = try Data(contentsOf: url)
+                   return data
+               } catch let error {
+                   fatalError("couldn't find data \(error)")
+               }
+           }
+    func testMuseumModel () {
+            let data = MuseumModel() ?? Data()
+            
+            do {
+                let museumData = try MuseumWrapper.decodeObjectFromData(from: data)
+                
+                 XCTAssertTrue(museumData.count > 0, "objects not loaded")
+            } catch let error {
+                XCTFail(error.localizedDescription)
+            }
+    }
+    
+    private func ArtObjectModel() -> Data? {
+                  let bundle = Bundle(for: type(of: self))
+                  guard let pathToData = bundle.path(forResource: "ArtObjectSample", ofType: ".json")  else {
+                      XCTFail("couldn't find Json")
+                      return nil
+                  }
+                  let url = URL(fileURLWithPath: pathToData)
+                  do {
+                      let data = try Data(contentsOf: url)
+                      return data
+                  } catch let error {
+                      fatalError("couldn't find data \(error)")
+                  }
+              }
+       func testArtObjectModel () {
+               let data = ArtObjectModel() ?? Data()
+               
+               do {
+                let objectData = try ArtObjectWrapper.decodeDetailsFromData(from: data)
+                XCTAssert(type(of: objectData) == ArtObjectDetail.self , "You do not have an ArtObjectDetail")
+                
+               } catch let error {
+                   XCTFail(error.localizedDescription)
+               }
+       }
+
 
 }
